c75ad0fdb2f8f81d7bff5e894326e12c423ee250
zakariaSenousy <zakaria.senousy@gmail.com>
2021-05-09 13:18:50 +0100

    Add files via upload

---
+++ Model B/src/networks_P2_1.py
@@ -0,0 +1,162 @@
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+class BaseNetwork(nn.Module):
+    def __init__(self, name, channels=1):
+        super(BaseNetwork, self).__init__()
+        self._name = name
+        self._channels = channels
+
+    def name(self):
+        return self._name
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+
+
+class PatchWiseNetwork(BaseNetwork):
+    def __init__(self, channels=1):
+        super(PatchWiseNetwork, self).__init__('pw' + str(channels), channels)
+        
+        self.features = nn.Sequential(
+            # Block 1
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=2, stride=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace = True),
+
+            # Block 2
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=2, stride=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace = True),
+
+            # Block 3
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=2, stride=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace = True),
+
+            # Block 4
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace = True),
+
+            # Block 5
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace = True),
+
+            nn.Conv2d(in_channels=256, out_channels=channels, kernel_size=1, stride=1),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(channels * 28 * 28, 3),
+        )
+ 
+        self.initialize_weights()
+        
+    def forward(self, x):
+        x = self.features(x)
+                    
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        x = F.log_softmax(x, dim=1)
+        
+        return x
+
+
+
+class ImageWiseNetwork(BaseNetwork):
+    def __init__(self, channels=1):
+        super(ImageWiseNetwork, self).__init__('iw' + str(channels), channels)
+
+        self.features = nn.Sequential(
+            
+            nn.Conv2d(in_channels= 9 * channels , out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=2, stride=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+
+            # Block 2
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=2, stride=2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+                        
+
+            nn.Conv2d(in_channels=128, out_channels=1, kernel_size=1, stride=1),
+        )
+
+        self.fc1 = nn.Linear(1 * 14 * 7, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 3)
+        self.dropout = nn.Dropout(p = 0.5)
+        
+        
+        self.initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(self.dropout(x)))
+        x = F.relu(self.fc3(self.dropout(x)))
+        y = self.fc4(self.dropout(x))
+
+        x = F.log_softmax((y), dim=1)
+        return x
